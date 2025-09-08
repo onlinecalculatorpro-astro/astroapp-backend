356091e55f1bedb0471e4261bf090900f8e1a5e1
onlinecalculatorpro-astro <onlinecalculatorpro@gmail.com>
2025-09-08 20:28:21 +0530

    Update __init__.py

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,1 +1,12 @@
+# app/core/__init__.py
+from .predictive import (
+    AspectSpec, AspectKind, MAJOR_ASPECTS, MINOR_ASPECTS,
+    TransitEngine, TransitEvent, PredictionResult, find_transits_in_range,
+    DashaPeriod, vimsottari_dasha,
+    compute_vargas_for_point, compute_vargas,
+    detect_yogas, house_index_for_longitude,
+    compute_houses, timescales_from_civil,
+    EvalResult, evaluate_univariate, permutation_pvalue_corr, bh_fdr, holdout_replicate,
+    feature_transit_proximity, feature_dasha_lords_onehot, feature_yoga_flags,
+)
 
